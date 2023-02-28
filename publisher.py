eb04aa6831f8083ef6b06b937990f21e27ff57a9
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 16:09:55 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -2,12 +2,31 @@ import zmq
 import threading
 import constPS
 
+class TopicRegistry:
+    def __init__(self):
+        self.topics = {}
+
+    def add_subscriber(self, topic, address):
+        if topic not in self.topics:
+            self.topics[topic] = []
+        self.topics[topic].append(address)
+
+    def remove_subscriber(self, topic, address):
+        if topic in self.topics and address in self.topics[topic]:
+            self.topics[topic].remove(address)
+            if not self.topics[topic]:
+                del self.topics[topic]
+
+    def get_subscribers(self, topic):
+        return self.topics.get(topic, [])
+
 # Servidor de chat
 class ChatServer:
     def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind("tcp://%s:%s" % (constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT))
+        self.topic_registry = TopicRegistry()
 
     def run(self):
         while True:
@@ -20,38 +39,20 @@ class ChatServer:
                 self.handle_topic_msg(msg_pack)
 
     def handle_user_msg(self, msg_pack):
-        dest = msg_pack[1]
+        topic = msg_pack[1]
         src = msg_pack[2]
         msg = msg_pack[3]
-        # Verifica se o destinatário está registrado no servidor
-        try:
-            dest_addr = constPS.registry[dest]
-        except:
-            self.socket.send(pickle.dumps(("ACK", "Destination user not found")))
-            return
-        # Cria uma conexão com o cliente destinatário
-        client_sock = self.context.socket(zmq.REQ)
-        dest_ip = dest_addr[0]
-        dest_port = dest_addr[1]
-        # Tenta se conectar ao cliente destinatário
-        try:
-            client_sock.connect("tcp://%s:%s" % (dest_ip, dest_port))
-        except:
-            self.socket.send(pickle.dumps(("ACK", "Destination user is down")))
-            client_sock.close()
-            return
-        # Envia a mensagem para o cliente destinatário e aguarda um ACK
-        msg_pack = (src, msg)
-        marshaled_msg_pack = pickle.dumps(msg_pack)
-        client_sock.send(marshaled_msg_pack)
-        marshaled_reply = client_sock.recv()
-        reply = pickle.loads(marshaled_reply)
-        if reply != "ACK":
-            self.socket.send(pickle.dumps(("ACK", "Destination user did not receive message properly")))
-        else:
-            self.socket.send(pickle.dumps(("ACK", "Message sent successfully")))
-
-        client_sock.close()
+        # Publica a mensagem no tópico
+        topic_socket = self.context.socket(zmq.PUB)
+        topic_socket.bind("tcp://%s:%s" % (constPS.TOPIC_SERVER_HOST, constPS.TOPIC_SERVER_PORT))
+        topic_socket.send_string("%s %s:%s %s" % (topic, constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT, pickle.dumps((src, msg))))
+        # Adiciona os endereços dos clientes inscritos ao tópico
+        subscribers = self.topic_registry.get_subscribers(topic)
+        for address in subscribers:
+            topic_socket.connect("tcp://%s:%s" % address)
+        # Publica a mensagem para os clientes inscritos e fecha o socket
+        topic_socket.send_string("%s %s:%s %s" % (topic, constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT, pickle.dumps((src, msg))))
+        topic_socket.close()
 
     def handle_topic_msg(self, msg_pack):
         cmd = msg_pack[1]
