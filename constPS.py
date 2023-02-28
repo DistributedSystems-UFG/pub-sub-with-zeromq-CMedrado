156d35e294dbc670a80e6221c350af4cd960c660
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 13:13:25 -0300

    Update constPS.py

--- constPS.py
+++ constPS.py
@@ -7,15 +7,10 @@ PUBLISHER_PORT = 5001
 TOPIC_SERVER_HOST = "127.0.0.1"
 TOPIC_SERVER_PORT = 5002
 
-REGISTRY = {
+registry = {
     'alice': ("172.31.48.169", 12001),
     'bob': ("172.31.48.169", 12002),
     'charlie': ("172.31.48.169", 12003),
     'david': ("172.31.48.169", 12004),
     'eve': ("172.31.48.169", 12005)
-}
-
-TOPICS = {
-    'tech': [],
-    'sports': []
 }
