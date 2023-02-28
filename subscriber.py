77014c0db982c82af343fd93d6190e7314ae8c10
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 13:11:10 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -1,54 +1,68 @@
 import zmq
-import time
+import threading
 import constPS
 
-
-class Client:
-    def __init__(self, name):
-        self.name = name
+class Subscriber:
+    def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
-        self.socket.setsockopt(zmq.SUBSCRIBE, b"")
-        self.socket.connect("tcp://%s:%s" % (constPS.PUBLISHER_HOST, constPS.PUBLISHER_PORT))
 
-    def subscribe_to_topic(self, topic):
-        self.socket.setsockopt(zmq.SUBSCRIBE, topic.encode())
+    def subscribe(self, topic):
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+        self.socket.connect("tcp://%s:%s" % (constPS.TOPIC_SERVER_HOST, constPS.TOPIC_SERVER_PORT))
 
-    def receive_messages(self):
+    def run(self):
         while True:
-            msg = self.socket.recv()
-            print(msg.decode())
-
-    def send_message_to_user(self, dest, message):
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://%s:%s" % (constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT))
-        msg_pack = (message, dest, self.name)
-        marshaled_msg_pack = pickle.dumps(msg_pack)
-        socket.send(marshaled_msg_pack)
-        marshaled_reply = socket.recv()
-        reply = pickle.loads(marshaled_reply)
-        if reply != "ACK":
-            print("Error: Server did not accept the message (dest does not exist?)")
-        else:
-            print("Message sent successfully!")
-        socket.close()
-
+            message = self.socket.recv_string()
+            topic, message = message.split(" ", 1)
+            print("TOPIC MESSAGE: %s - %s" % (topic, message))
 
 if __name__ == "__main__":
-    client_name = input("Enter your name: ")
-    client = Client(client_name)
+    subscriber = Subscriber()
 
     while True:
-        msg_type = input("Enter message type (1 - individual, 2 - topic): ")
-        if msg_type == "1":
-            dest = input("Enter destination user: ")
-            message = input("Enter message: ")
-            client.send_message_to_user(dest, message)
-        elif msg_type == "2":
-            topic = input("Enter topic to subscribe: ")
-            client.subscribe_to_topic(topic)
-            print("Subscribed to topic %s" % topic)
-            client.receive_messages()
+        dest_type = input("Enter destination type (user or topic): ")
+        if dest_type == "user":
+            dest = input("Enter username: ")
+            msg = input("Enter message: ")
+            # Envia mensagem individual
+            client_sock = subscriber.context.socket(zmq.REQ)
+            try:
+                dest_addr = constPS.registry[dest]
+            except:
+                print("Destination user not found")
+                continue
+            dest_ip = dest_addr[0]
+            dest_port = dest_addr[1]
+            try:
+                client_sock.connect("tcp://%s:%s" % (dest_ip, dest_port))
+            except:
+                print("Destination user is down")
+                client_sock.close()
+                continue
+            msg_pack = (dest_type, dest, constPS.CLIENT_NAME, msg)
+            marshaled_msg_pack = pickle.dumps(msg_pack)
+            client_sock.send(marshaled_msg_pack)
+            marshaled_reply = client_sock.recv()
+            reply = pickle.loads(marshaled_reply)
+            if reply != "ACK":
+                print("Destination user did not receive message properly")
+            else:
+                print("Message sent successfully")
+            client_sock.close()
+        elif dest_type == "topic":
+            topic = input("Enter topic: ")
+            msg = input("Enter message: ")
+            # Publica mensagem em tópico
+            topic_socket = subscriber.context.socket(zmq.PUB)
+            topic_socket.connect("tcp://%s:%s" % (constPS.TOPIC_SERVER_HOST, constPS.TOPIC_SERVER_PORT))
+            msg_pack = (dest_type, topic, constPS.CLIENT_NAME, msg)
+            marshaled_msg_pack = pickle.dumps(msg_pack)
+            topic_socket.send_string("%s %s:%s %s" % (topic, constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT, marshaled_msg_pack))
+            topic_socket.close()
         else:
-            print("Invalid message type. Try again.")
+            print("Invalid destination type")
+            continue
+
+    subscriber_thread = threading.Thread(target=subscriber.run)
+    subscriber_thread.start()
