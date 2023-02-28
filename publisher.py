340b14b8d735da98ad0be683a7586cd919181d32
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 12:04:47 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,125 +1,69 @@
 import zmq
-import threading
-import constPS
-import pickle
+import time
+from constPS import *
 
 context = zmq.Context()
-s = context.socket(zmq.PUB)  # create a publisher socket
-p = "tcp://" + constPS.HOST + ":" + str(constPS.PORT)  # how and where to communicate
-s.bind(p)  # bind socket to the address
 
+# Socket publisher
+s = context.socket(zmq.PUB)
+p = "tcp://{}:{}".format(HOST, PORT)
+s.bind(p)
 
-class RecvHandler(threading.Thread):
-    def __init__(self, sock):
-        threading.Thread.__init__(self)
-        self.client_socket = sock
+# Socket RPC
+rpc_socket = context.socket(zmq.REP)
+rpc_socket.bind("tcp://*:{}".format(RPC_PORT))
 
-    def run(self):
-        while True:
-            (conn, addr) = self.client_socket.accept()
-            marshaled_msg_pack = conn.recv(1024)
-            msg_pack = pickle.loads(marshaled_msg_pack)
-            print("MESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1])
-            conn.send(pickle.dumps("ACK"))
-            conn.close()
-        return
-
-
-def handle_client(conn, addr):
-    # Recebe a mensagem do cliente
-    marshaled_msg_pack = conn.recv(1024)
-    msg_pack = pickle.loads(marshaled_msg_pack)
-    msg = msg_pack[0]
-    dest = msg_pack[1]
-    src = msg_pack[2]
-    # Imprime informações sobre a mensagem
-    print("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest)
-
-    # Verifica se é uma mensagem para um tópico ou uma mensagem individual
-    if dest.startswith("#"):  # se começa com "#" é mensagem para um tópico
-        s.send(str.encode(dest + " " + msg))
-    else:  # caso contrário é uma mensagem individual
-        # Verifica se o destinatário está registrado no servidor
-        try:
-            dest_addr = constPS.registry[dest]
-        except:
-            conn.send(pickle.dumps("NACK"))
-            conn.close()
-            return
-
-        # Cria uma conexão com o cliente destinatário
-        client_sock = context.socket(zmq.REQ)
-        dest_ip = dest_addr[0]
-        dest_port = dest_addr[1]
-
-        # Tenta se conectar ao cliente destinatário
-        try:
-            client_sock.connect("tcp://" + dest_ip + ":" + str(dest_port))
-        except:
-            print("Error: Destination client is down")
-            client_sock.close()
-            return
-
-        # Envia a mensagem para o cliente destinatário e aguarda um ACK
-        msg_pack = (msg, src)
-        marshaled_msg_pack = pickle.dumps(msg_pack)
-        client_sock.send(marshaled_msg_pack)
-        marshaled_reply = client_sock.recv()
-        reply = pickle.loads(marshaled_reply)
-        if reply != "ACK":
-            print("Error: Destination client did not receive message properly")
-
-        # Fecha a conexão com o cliente destinatário
-        client_sock.close()
-
-    # Envia uma mensagem de ACK ao cliente que enviou a mensagem
-    conn.send(pickle.dumps("ACK"))
-    conn.close()
-
-
-server_sock = context.socket(zmq.REP)
-server_sock.bind("tcp://" + constPS.HOST + ":" + str(constPS.PORT))
-
-# Put receiving thread to run
-recv_handler = RecvHandler(server_sock)
-recv_handler.start()
-
-print("Chat Server is ready...")
+# Dicionário para armazenar as conexões dos clientes
+clients = {}
 
 while True:
-    cmd = input("Enter command (1 for individual message, 2 for topic message): ")
-    if cmd == "1":
-        dest = input("Enter destination: ")
-        msg = input("Enter message: ")
-        msg_pack = (msg, dest, "server")
+    # Recebe o comando do cliente
+    command = rpc_socket.recv_json()
 
-        # Cria uma conexão com o servidor
-        client_sock = context.socket(zmq.REQ)
-        server_ip = constPS.registry["server"][0]
-        server_port = constPS.registry["server"][1]
+    # Verifica o tipo de mensagem
+    if command['type'] == 'individual':
+        # Envia mensagem individual
+        dest = command['dest']
+        message = command['message']
 
-        # Tenta se conectar ao servidor
-        try:
-            client_sock.connect("tcp://" + server_ip + ":" + str(server_port))
-        except:
-            print("Error: Server is down")
-            client_sock.close()
-            continue
+        if dest not in clients:
+            rpc_socket.send_json({'status': 'error', 'message': 'User not found'})
+        else:
+            dest_socket = clients[dest]
+            dest_socket.send_string(message)
+            rpc_socket.send_json({'status': 'ok'})
 
-        # Envia a mensagem para o servidor e aguarda um ACK
-        marshaled_msg_pack = pickle.dumps(msg_pack)
-        client_sock.send(marshaled_msg_pack)
-        marshaled_reply = client_sock.recv()
-        reply = pickle.loads(marshaled_reply)
-        if reply != "ACK":
-            print("Error: Server did not accept the message (dest does not exist?)")
+    elif command['type'] == 'topic':
+        # Envia mensagem para o tópico
+        topic = command['topic']
+        message = command['message']
 
-        # Fecha a conexão com o servidor
-        client_sock.close()
+        s.send_string("{} {}".format(topic, message))
+        rpc_socket.send_json({'status': 'ok'})
 
-    elif cmd == "2":
-        topic = input("Enter topic: ")
-        msg = input("Enter message: ")
-        s.send(str.encode("#" + topic + " " + msg))
+    elif command['type'] == 'register':
+        # Registra um novo cliente
+        username = command['username']
+        client_socket = context.socket(zmq.PAIR)
+        client_socket.bind("inproc://{}".format(username))
+        clients[username] = client_socket
+        rpc_socket.send_json({'status': 'ok'})
+
+    elif command['type'] == 'exit':
+        # Remove um cliente registrado
+        username = command['username']
+        if username in clients:
+            client_socket = clients[username]
+            client_socket.close()
+            del clients[username]
+            rpc_socket.send_json({'status': 'ok'})
+        else:
+            rpc_socket.send_json({'status': 'error', 'message': 'User not found'})
+
     else:
-        print("Invalid command. Please try again.")
+        rpc_socket.send_json({'status': 'error', 'message': 'Invalid command'})
+
+    # Publica a hora atual a cada 5 segundos
+    current_time = time.asctime()
+    s.send_string("TIME {}".format(current_time))
+    time.sleep(5)
