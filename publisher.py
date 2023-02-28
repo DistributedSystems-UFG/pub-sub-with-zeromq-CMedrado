d0d92fe83f53d4284349e3f3f0f3c1982ef2ad97
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 10:37:16 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -2,10 +2,15 @@ import zmq, time
 from constPS import * #-
 
 context = zmq.Context()
-s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
-s.bind(p)                          # bind socket to the address
+s = context.socket(zmq.PUB)  # create a publisher socket
+p = "tcp://" + HOST + ":" + PORT  # how and where to communicate
+s.bind(p)  # bind socket to the address
+
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+    topic = input("Enter topic (or leave blank to publish to all subscribers): ")
+    message = input("Enter message: ")
+    if topic:
+        s.send_multipart([str.encode(topic), str.encode(message)])
+    else:
+        s.send(str.encode(message))  # publish the current time
+	
