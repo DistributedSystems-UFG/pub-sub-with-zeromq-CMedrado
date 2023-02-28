4484864c0d271388e67a6cd4a38d31e4f48d7ec1
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 13:06:13 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,85 +1,104 @@
 import zmq
 import threading
 import constPS
-import pickle
 
-class Server(threading.Thread):
+
+# Servidor de chat
+class ChatServer:
     def __init__(self):
-        threading.Thread.__init__(self)
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind("tcp://%s:%s" % (constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT))
-        print("Chat server is running...")
 
     def run(self):
         while True:
-            # Recebe mensagem do cliente
             marshaled_msg_pack = self.socket.recv()
             msg_pack = pickle.loads(marshaled_msg_pack)
-            msg_type = msg_pack[0]
-            msg = msg_pack[1]
-            dest = msg_pack[2]
-            src = msg_pack[3]
+            dest_type = msg_pack[0]
+            if dest_type == "user":
+                self.handle_user_msg(msg_pack)
+            elif dest_type == "topic":
+                self.handle_topic_msg(msg_pack)
 
-            # Mensagem individual
-            if msg_type == "I":
-                # Encontra o IP e porta do destinatário
-                try:
-                    dest_addr = constPS.registry[dest]
-                except:
-                    self.socket.send(pickle.dumps("NACK"))
-                    continue
+    def handle_user_msg(self, msg_pack):
+        dest = msg_pack[1]
+        src = msg_pack[2]
+        msg = msg_pack[3]
+        # Verifica se o destinatário está registrado no servidor
+        try:
+            dest_addr = constPS.registry[dest]
+        except:
+            self.socket.send(pickle.dumps(("ACK", "Destination user not found")))
+            return
+        # Cria uma conexão com o cliente destinatário
+        client_sock = self.context.socket(zmq.REQ)
+        dest_ip = dest_addr[0]
+        dest_port = dest_addr[1]
+        # Tenta se conectar ao cliente destinatário
+        try:
+            client_sock.connect("tcp://%s:%s" % (dest_ip, dest_port))
+        except:
+            self.socket.send(pickle.dumps(("ACK", "Destination user is down")))
+            client_sock.close()
+            return
+        # Envia a mensagem para o cliente destinatário e aguarda um ACK
+        msg_pack = (src, msg)
+        marshaled_msg_pack = pickle.dumps(msg_pack)
+        client_sock.send(marshaled_msg_pack)
+        marshaled_reply = client_sock.recv()
+        reply = pickle.loads(marshaled_reply)
+        if reply != "ACK":
+            self.socket.send(pickle.dumps(("ACK", "Destination user did not receive message properly")))
+        else:
+            self.socket.send(pickle.dumps(("ACK", "Message sent successfully")))
 
-                # Cria conexão com o destinatário e envia a mensagem
-                try:
-                    client_sock = self.context.socket(zmq.REQ)
-                    client_sock.connect("tcp://%s:%s" % (dest_addr[0], dest_addr[1]))
-                    msg_pack = ("I", msg, src)
-                    marshaled_msg_pack = pickle.dumps(msg_pack)
-                    client_sock.send(marshaled_msg_pack)
-                    marshaled_reply = client_sock.recv()
-                    reply = pickle.loads(marshaled_reply)
-                    client_sock.close()
-                except:
-                    reply = "NACK"
+        client_sock.close()
 
-            # Mensagem para tópico
-            elif msg_type == "T":
-                # Envia a mensagem para todos os assinantes do tópico
-                try:
-                    publisher_sock = self.context.socket(zmq.PUB)
-                    publisher_sock.bind("tcp://%s:%s" % (constPS.PUBLISHER_HOST, constPS.PUBLISHER_PORT))
-                    msg_pack = ("T", msg)
-                    marshaled_msg_pack = pickle.dumps(msg_pack)
-                    publisher_sock.send_multipart([bytes(dest, "utf-8"), marshaled_msg_pack])
-                    publisher_sock.close()
-                    reply = "ACK"
-                except:
-                    reply = "NACK"
-
-            # Responde ao cliente
-            self.socket.send(pickle.dumps(reply))
+    def handle_topic_msg(self, msg_pack):
+        topic = msg_pack[1]
+        src = msg_pack[2]
+        msg = msg_pack[3]
+        # Publica a mensagem no tópico
+        topic_socket = self.context.socket(zmq.PUB)
+        topic_socket.bind("tcp://%s:%s" % (constPS.TOPIC_SERVER_HOST, constPS.TOPIC_SERVER_PORT))
+        topic_socket.send_string(
+            "%s %s:%s %s" % (topic, constPS.CHAT_SERVER_HOST, constPS.CHAT_SERVER_PORT, pickle.dumps((src, msg))))
+        topic_socket.close()
 
 
-class Publisher(threading.Thread):
+# Servidor de tópicos
+class TopicServer:
     def __init__(self):
-        threading.Thread.__init__(self)
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUB)
-        self.socket.bind("tcp://%s:%s" % (constPS.PUBLISHER_HOST, constPS.PUBLISHER_PORT))
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self.socket.bind("tcp://%s:%s" % (constPS.TOPIC_SERVER_HOST, constPS.TOPIC_SERVER_PORT))
 
     def run(self):
         while True:
-            dest = input("Enter topic to publish message to: ")
-            msg = input("Enter message: ")
-            msg_pack = ("T", msg)
-            marshaled_msg_pack = pickle.dumps(msg_pack)
-            self.socket.send_multipart([bytes(dest, "utf-8"), marshaled_msg_pack])
+            message = self.socket.recv_string()
+            topic, message = message.split(" ", 1)
+            print("TOPIC MESSAGE: %s - FROM: %s" % (message, topic))
+            # Envia a mensagem para todos os assinantes do tópico
+            sub_socket = self.context.socket(zmq.REQ)
+            for addr in constPS.topic_subscribers.get(topic, []):
+                try:
+                    sub_socket.connect("tcp://%s:%s" % addr)
+                    sub_socket.send(message.encode())
+                    sub_socket.recv()
+                except:
+                    print("Error: Subscriber %s is down" % str(addr))
+            sub_socket.close()
 
 
-if __name__ == "__main__":
-    # Inicializa servidor e publicador
-    server = Server()
-    server.start()
-    publisher = Publisher()
-    publisher.start()
+if name == "main":
+    chat_server = ChatServer()
+    topic_server = TopicServer()
+    chat_thread = threading.Thread(target=chat_server.run)
+    topic_thread = threading.Thread(target=topic_server.run)
+
+    chat_thread.start()
+    topic_thread.start()
+
+    chat_thread.join()
+    topic_thread.join()
