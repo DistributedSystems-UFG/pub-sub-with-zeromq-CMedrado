2fe341b90de8db2e3c4a505d13b386538a598e0a
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-28 11:23:08 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -1,12 +1,45 @@
 import zmq
-from constPS import HOST, PORT
+import threading
+import constPS
 
-context = zmq.Context()
-s = context.socket(zmq.SUB)  # create a subscriber socket
-p = "tcp://" + HOST + ":" + PORT  # how and where to communicate
-s.connect(p)  # connect socket to the address
-s.setsockopt_string(zmq.SUBSCRIBE, "")  # subscribe to all topics
+# Subscriber
+def subscribe():
+    context = zmq.Context()
+    s = context.socket(zmq.SUB)
+    s.connect("tcp://" + constPS.HOST + ":" + str(constPS.PORT))
+    s.setsockopt(zmq.SUBSCRIBE, b'#')  # subscribe to all topics
 
-while True:
-    message = s.recv_multipart()
-    print(f"Received {message[1].decode()} from topic {message[0].decode()}")
+    while True:
+        msg = s.recv()
+        print("TOPIC MSG: " + msg.decode("utf-8"))
+
+# Client
+def send_msg():
+    context = zmq.Context()
+    c = context.socket(zmq.REQ)
+    c.connect("tcp://" + constPS.HOST + ":" + str(constPS.PORT))
+
+    while True:
+        cmd = input("Enter command (1 for individual message, 2 for topic message): ")
+        if cmd == "1":
+            dest = input("Enter destination: ")
+            msg = input("Enter message: ")
+            msg_pack = (msg, dest, "client")
+            marshaled_msg_pack = pickle.dumps(msg_pack)
+            c.send(marshaled_msg_pack)
+            marshaled_reply = c.recv()
+            reply = pickle.loads(marshaled_reply)
+            if reply != "ACK":
+                print("Error: Server did not accept the message (dest does not exist?)")
+        elif cmd == "2":
+            topic = input("Enter topic: ")
+            msg = input("Enter message: ")
+            s.send(str.encode("#" + topic + " " + msg))
+        else:
+            print("Invalid command")
+
+# Start threads for subscriber and client
+t1 = threading.Thread(target=subscribe)
+t1.start()
+t2 = threading.Thread(target=send_msg)
+t2.start()
